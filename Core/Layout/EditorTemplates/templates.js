55752ce0f32a0583302701d66945cd246dab188e
Jesse Dobbelaere <jesse@dobbelaere-ae.be>
2014-09-15 02:18:54 +0200

    Fix editortemplates path

--- Core/Layout/EditorTemplates/templates.js
+++ Core/Layout/EditorTemplates/templates.js
@@ -2,21 +2,21 @@
 	{
 		"title": "Blue list",
 		"description": "List snippet",
-		"image": "/frontend/themes/graydon/core/layout/editor_templates/images/bluelist.png",
+		"image": "/src/Frontend/Themes/triton/Core/Layout/EditorTemplates/images/bluelist.png",
 		"html": "<div class=\"navBlock clearfix\"><h3>This is a title</h3><ul><li>Item 1</li><li>Item 2</li><li>Item 3</li><li>Item 4</li></ul></div>"
 	},
 	
 	{
 		"title": "Grey text",
 		"description": "Call-to-action snippet",
-		"image": "/frontend/themes/graydon/core/layout/editor_templates/images/cta_gray.png",
+		"image": "/src/Frontend/Themes/triton/Core/Layout/EditorTemplates/images/cta_gray.png",
 		"html": "<div class=\"navBlockGray clearfix\"><h3>This is a title</h3><p>This is the content that explains the call to action button</p><a class=\"subAll\" href=\"/\">Button</a></div>"
 	},
 
 	{
 		"title": "E-mail Blue Block",
 		"description": "Blue block for usage in E-mail",
-		"image": "/frontend/themes/graydon/core/layout/editor_templates/images/cta_mail.png",
+		"image": "/src/Frontend/Themes/triton/Core/Layout/EditorTemplates/images/cta_mail.png",
 		"html": "<div class='mailCta'><h2>This is a title</h2><p>This is the content</p></div><p>;&nbsp</p>"
 	}
 ]
